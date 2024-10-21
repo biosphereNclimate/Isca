c5fe793ffc424a4ef940f5e0830a05aaf23d49ad
Paul Hall <paul_hall@brown.edu>
2024-10-21 15:22:36 -0400

    Updated netcdf library, changed paths to use jobtmp for output

--- exp/site_specific/oscar/run_axisymmetric.sh
+++ exp/site_specific/oscar/run_axisymmetric.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #**********************
-# Example batch script for running isca's held_suarez test
+# Example batch script for running isca's axisymmetric test
 # case on Oscar
 #**********************
 # SLURM JOB INFORMATION
@@ -8,11 +8,11 @@
 # Walltime requested for job (24 hrs)
 #SBATCH -t 24:00:00
 
-# Request use of 8 cores and 8GB of memory per core on 1 node
+# Request use of 8 cores and 4GB of memory per core on 1 node
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=8
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=8G
+#SBATCH --mem-per-cpu=4G
 
 # Define Oscar partition to use
 #SBATCH -p batch
@@ -35,13 +35,13 @@
 ulimit -s unlimited
 # load the required modules
 module load hpcx-mpi/4.1.5rc2s-yflad4v
-module load netcdf-fortran-mpi/4.6.0-ciymq6f
+module load netcdf-mpi/4.9.2-og6h2km
 
 # set environment variables (change to suit your needs)
 export GFDL_BASE=/oscar/home/$USER/data/$USER/isca.venv/Isca
 export GFDL_ENV=oscar
-export GFDL_WORK=/oscar/scratch/$USER/isca/work
-export GFDL_DATA=/oscar/scratch/$USER/isca/data
+export GFDL_WORK=/sss/jobtmp/$USER/isca/work
+export GFDL_DATA=/sss/jobtmp/$USER/isca/data
 # activate environment
 source $GFDL_BASE/../bin/activate
 # run the case
